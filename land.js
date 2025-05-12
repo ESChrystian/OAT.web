c8dd0f02dc6f82dc84936b4badab4473b61fbbc3
Chrystian de Almeida Siva <21520230022@maristabrasil.g12.br>
2025-05-12 08:17:34 -0300

    Insere pesquisas do Forms

--- land.js
+++ land.js
@@ -66,18 +66,6 @@ ScrollReveal().reveal('.box-3', {
 
 });
 
-const openModalButton = document.querySelector("#open-modal");
-const closeModalButton = document.querySelector("#close-modal");
-const modal = document.querySelector("#modal");
-const fade = document.querySelector("#fade");
-const toggleModal = () => {
-    modal.classList.toggle("hide");
-    fade.classList.toggle("hide");
-};
-[openModalButton, closeModalButton, fade].forEach((el) => {
-    el.addEventListener("click", () => toggleModal());
-});
-
 new Swiper('.card-wrapper', {
 
     loop: true,
